ac584032b4a0ac81aeccd3d652c6a5a17ccf7dbc
Visman <visman2008@googlemail.com>
2022-12-10 17:14:18 +0700

    Обновлен список акселераторов PHP

--- src/admin/index.php
+++ src/admin/index.php
@@ -120,18 +120,22 @@ if (in_array($db_type, array('mysql', 'mysqli', 'mysql_innodb', 'mysqli_innodb')
 }
 
 // Check for the existance of various PHP opcode caches/optimizers
-if (function_exists('mmcache'))
-    $php_accelerator = '<a href="http://turck-mmcache.sourceforge.net/">Turck MMCache</a>';
+if (ini_get('opcache.enable') && function_exists('opcache_invalidate'))
+    $php_accelerator = '<a href="https://www.php.net/opcache/">Zend OPcache</a>';
+else if (ini_get('wincache.fcenabled'))
+    $php_accelerator = '<a href="https://www.php.net/wincache/">Windows Cache for PHP</a>';
+else if (function_exists('mmcache'))
+    $php_accelerator = '<a href="https://sourceforge.net/projects/turck-mmcache/">Turck MMCache</a>';
 else if (isset($_PHPA))
-    $php_accelerator = '<a href="http://www.php-accelerator.co.uk/">ionCube PHP Accelerator</a>';
+    $php_accelerator = '<a href="https://www.ioncube.com/">ionCube PHP Accelerator</a>';
 else if (ini_get('apc.enabled'))
-    $php_accelerator ='<a href="http://www.php.net/apc/">Alternative PHP Cache (APC)</a>';
+    $php_accelerator ='<a href="https://web.archive.org/web/20160324235630/http://www.php.net/apc/">Alternative PHP Cache (APC)</a>';
 else if (ini_get('zend_optimizer.optimization_level'))
     $php_accelerator = '<a href="http://www.zend.com/products/zend_optimizer/">Zend Optimizer</a>';
 else if (ini_get('eaccelerator.enable'))
     $php_accelerator = '<a href="http://eaccelerator.net/">eAccelerator</a>';
 else if (ini_get('xcache.cacher'))
-    $php_accelerator = '<a href="http://xcache.lighttpd.net/">XCache</a>';
+    $php_accelerator = '<a href="https://web.archive.org/web/20120224193029/http://xcache.lighttpd.net/">XCache</a>';
 else
     $php_accelerator = $lang_admin_index['Not applicable'];
 
